6bfc2449fbf2f0aead4ab0ea46888eb4c0038335
jorisChalumeau <joris.chalumeau@gmail.com>
2017-05-30 11:33:35 +0200

    enlever prints inutiles

--- src/modele/joueurs/JoueurIAMoyen.java
+++ src/modele/joueurs/JoueurIAMoyen.java
@@ -347,7 +347,7 @@ public class JoueurIAMoyen extends JoueurIA {
 				try {
 					partie.executerAction(move.src, move.dest);
 					listeCoups.add(move);
-					System.out.println(move.src.getRow()+", "+move.src.getColumn()+" ; "+move.dest.getRow()+", "+move.dest.getColumn());
+					
 					// après chaque coup on vérifie si la partie est finie
 					if (partie.gagnantPartie() != null || listeCoups.size() == 3)
 						break;
@@ -356,7 +356,6 @@ public class JoueurIAMoyen extends JoueurIA {
 			}
 		}
 
-		partie.getPlateau().Afficher();
 		return listeCoups;
 	}
 
